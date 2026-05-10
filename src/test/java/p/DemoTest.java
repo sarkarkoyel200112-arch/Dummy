12c09136eebf962e25069d94b1705b933a68e4b5
Koyel Sarkar <sarkarkoyel200112@gmail.com>
2026-05-10 10:54:46 +0530

    first commit

---
+++ src/test/java/p/DemoTest.java
@@ -0,0 +1,13 @@
+package p;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void demoTest()
+	{
+		System.out.println("Mango");
+		System.out.println("Apple");
+	}
+
+}
